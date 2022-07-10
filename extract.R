b5b88a1c626cf0cb94ad81d1ed0478d244f7b080
VascoRibeiroPereira <vascoribeiro.sintra@gmail.com>
2022-07-10 12:37:04 +0100

    Commented on the extraction code

--- extract.R
+++ extract.R
@@ -1,14 +1,14 @@
 library(tidyverse)
 library(httr)
 library(pdftools)
-setwd("R/pessoa")
 
 
 myStatus = integer()
 
 for (i in 1:5000) {
         
-        tmp = paste0("http://arquivopessoa.net/typographia/textos/arquivopessoa-",i,".pdf")
+        tmp = paste0(
+          "http://arquivopessoa.net/typographia/textos/arquivopessoa-",i,".pdf")
         myStatus = c(myStatus, GET(tmp)$status_code)
         
         Sys.sleep(.5)
@@ -17,15 +17,16 @@ for (i in 1:5000) {
 
 obras = tibble(url_id = 1:5000, status = myStatus)
 
-
-second_iteration = obras %>% filter(status == 503)
+## 503 stands for service unavailable, I double checked it
+second_iteration = obras %>% filter(status == 503) 
 
 
 myStatus2 = integer()
 
 for (i in second_iteration$url_id) {
         
-        tmp = paste0("http://arquivopessoa.net/typographia/textos/arquivopessoa-",i,".pdf")
+        tmp = paste0(
+          "http://arquivopessoa.net/typographia/textos/arquivopessoa-",i,".pdf")
         myStatus2 = c(myStatus2, GET(tmp)$status_code)
         
         Sys.sleep(1)
@@ -34,21 +35,27 @@ for (i in second_iteration$url_id) {
 
 obras_second = tibble(url_id = second_iteration$url_id, status = myStatus2)
 
+## getting all status 200 (OK) to download automatically next
 obras_completas = bind_rows(obras %>% filter(status == 200), 
                             obras_second %>% filter(status == 200)) %>% 
-        arrange(url_id)
+        arrange(url_id) 
 
 
 
-pdf_texts = list()
+pdf_texts = list() ## recursive download and read the pdf files into a list
 for (i in obras_completas$url_id) {
         
-        download.file(paste0("http://arquivopessoa.net/typographia/textos/arquivopessoa-",i,".pdf"), 
+        download.file(
+          paste0(
+            "http://arquivopessoa.net/typographia/textos/arquivopessoa-",
+            i,
+            ".pdf"), 
                       destfile = "sample.pdf", mode = "wb")
         pdf_texts[[i]] <- pdftools::pdf_text("sample.pdf")
         unlink("sample.pdf")
         Sys.sleep(1)
 }
 
-saveRDS(pdf_texts, file="rawData.RData")
+## save the complete list as RData for space econ
+saveRDS(pdf_texts, file="rawData.RData") 
 
